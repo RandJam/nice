adfd12e7eb980ebce92f2eae4995c7afd45a97f5
antcin <ciniglioantonio@gmail.com>
2018-05-03 21:44:53 +0100

    test rectangleButton; sketch home page screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './src/components/Button'
-import BasicFlatList from './src/components/BasicFlatList'
-import { StackNavigator } from 'react-navigation';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Home from './src/screens/Home';
 
-import ButtonScreen from './src/screens/ButtonScreen'
-import ListScreen from './src/screens/ListScreen'
-
-const Navigation = StackNavigator({
-  First: {screen: ButtonScreen},
-  Second: {screen: ListScreen}
-})
-
-export default Navigation;
+export default class App extends Component {
+  render() {
+    return (
+    <Home />
+    );
+  }
+}
---
+++ frontend/src/components/__tests__/RectangleButton.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme'; //Enzyme is a testing utility used for making assertions and manipulating React components
+import RectangleButton, { styles } from '../RectangleButton';
+
+describe('rendering',  () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<RectangleButton text='rectButt' handleOnPress={() => {}}/>)
+  })
+  it('renders a <TouchableHighlight/>', () => {
+    expect(wrapper.find('TouchableHighlight')).toHaveLength(1)
+  })
+})
+
+describe('interaction', () => {
+  let wrapper
+  let props
+  beforeEach(() => {
+    props = { text:'rectButt', color: '#FFFF00', handleOnPress: jest.fn() },
+    wrapper = shallow(<RectangleButton {...props} />)
+  })
+  describe('clicking the button', () => {
+    beforeEach(() => {
+      wrapper.find('TouchableHighlight').prop('onPress')()
+    })
+    it('calls the handleOnPress callback function', () => {
+      expect(props.handleOnPress).toHaveBeenCalledTimes(1)
+    })
+  })
+})
