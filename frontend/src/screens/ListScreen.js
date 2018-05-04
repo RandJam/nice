a46aa5a0140f63f955d4535e26991aa88a8352f8
danielcampos <ddregalo@gmail.com>
2018-05-04 12:26:28 +0100

    Creat button links to Just Giving Donate page

--- frontend/src/screens/ListScreen.js
+++ frontend/src/screens/ListScreen.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import {Platform, AppRegistry, Text, View, StyleSheet, TouchableHighlight, TouchableOpacity, TextInput } from 'react-native';
-import BasicFlatList from '../components/BasicFlatList'
+import {Platform, AppRegistry, Text, View, StyleSheet, TouchableHighlight, TouchableOpacity, TextInput, Image, Linking } from 'react-native';
+import BasicFlatList from '../components/BasicFlatList';
+import axios from 'axios';
+
 const util = require('util')
 
 export default class ListScreen extends Component{
   static navigationOptions = {
     title: 'List Screen',
   };
+
+  // api get to collect list of charities
+  // axios.get('')
+
   render(){
     console.log("this.props.navigation = " + util.inspect(this.props.navigation, false, null))
     var {params} = this.props.navigation.state;
@@ -14,7 +20,9 @@ export default class ListScreen extends Component{
       <View>
         <BasicFlatList />
         <Text> ListScreen </Text>
-
+        <Text onPress={() => Linking.openURL('https://link.justgiving.com/v1/charity/donate/charityId/247383?amount=10.00&currency=GBP&reference=test&exitUrl=http%3A%2F%2Fwww.ddregalo.com%3FjgDonationId%3DJUSTGIVING-DONATION-ID&message=test')}>
+        <Image source={ require('../assets/donate-button.png') } alt={"Donate with JustGiving"} />
+        </Text>
       </View>
     )
 
