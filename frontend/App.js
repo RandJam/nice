e0fb19bbd7cd9389d9d04b1b9a93313c2b9b424a
antcin <ciniglioantonio@gmail.com>
2018-05-11 13:00:01 +0100

    remove authscreen from app.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,6 @@
 import React, { Component }  from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import AuthScreen from './src/screens/AuthScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import ListScreen from './src/screens/ListScreen';
 import JgWebviewScreen from './src/screens/JgWebviewScreen';
